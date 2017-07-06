ab26cabefc7d88eb2e4b28b65d7840cc2c5a72bf
Mike Peiman <mikepeiman@gmail.com>
2017-07-06 16:18:24 -0700

    react synthetic event system, including mouse and touch events

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
-  render() {
-    return <Title text="12345"/>
+  constructor(){
+    super();
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this)
   }
-}
-
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  text(props, propName, component) {
-    if(!(propName in props)){
-      return new Error('missing ${propName}')
-    }
-  if(props[propName].length < 6) {
-    return new Error('${propName} was too short')
-    }
+  update(e){
+    this.setState({currentEvent: e.type})
+  }
+  render() {
+    return (
+      <div>
+        <textarea
+        cols="30" 
+        rows="10"
+        onKeyPress={this.update}
+        onCopy={this.update}
+        onCut={this.update}
+        onPaste={this.update}
+        onFocus={this.update}
+        onBlur={this.update}
+        onClick={this.update}
+        onDoubleClick={this.update}
+        onTouchStart={this.update}
+        onTouchMove={this.update}
+        onTouchEnd={this.update}
+        />
+          <h1>
+            {this.state.currentEvent}
+          </h1>
+      </div>
+    )
   }
 }
 
