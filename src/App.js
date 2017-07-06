6e59b963073d287ff973c72f26376018b6b99fdc
Mike Peiman <mikepeiman@gmail.com>
2017-07-06 15:15:16 -0700

    egghead.io tutorial on react

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state text',
+      cat: 0
+    }
+  }
+  update ( e ) {
+    this.setState({ 
+      txt: e.target.value,
+      cat: e.target.value })
+  }
+  render() {
+    let txt = this.state.txt;
+    let cat = this.state.cat;
+    return (
+      <div>
+        <h1>{ txt }</h1>
+        <Widget update={this.update.bind(this)} />
+      </div>
+    )
+  }
+}
+
+const Widget = (props) =>
+<input type-"text" onChange={props.update} />
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "this is the default txt"
+}
+
+// const App = () => <h1>Stateless function</h1>
+// stateless function
+
+export default App
