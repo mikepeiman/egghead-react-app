96c6d5e67abf756496c963a93ebbae9e4fbd77e2
Mike Peiman <mikepeiman@gmail.com>
2017-07-12 14:47:10 -0700

    Finally figured out how to use stylesheets, @import in app.js rather than link in index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bulma.css';
+import './font-awesome-4.7.0/css/font-awesome.css';
 
 class App extends React.Component {
   constructor() {
@@ -25,6 +27,8 @@ class App extends React.Component {
         update={this.update.bind(this)}
       /> {this.state.a}
       <hr />
+      <i className="fa fa-spinner fa-spin fa-3x fa-fw"></i> Icon
+      <hr />
       <input 
       ref={ node => this.b = node } // ref="b"
         type="text" 
@@ -38,7 +42,10 @@ class App extends React.Component {
 class Widget extends React.Component {
   render() {
     return (
-      <div><input ref="message" type="text" onChange={this.props.update} /></div>
+      <div>
+      <input ref="message" type="text" onChange={this.props.update} />
+      
+      </div>
     )
   }
 }
