e92f10a6e19e0c45bb090a274f4d2689a3fa5a19
Mike Peiman <mikepeiman@gmail.com>
2017-07-12 19:46:41 -0700

    Manage React component state with lifecycle methods

--- src/App.js
+++ src/App.js
@@ -6,73 +6,49 @@ import './font-awesome-4.7.0/css/font-awesome.css';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
+    this.state = {val: 0}
     this.update = this.update.bind(this)
   }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+  update() {
+    this.setState({val: this.state.val + 1})
+  }
+  componentWillMount() {
+    console.log('componentWillMount');
+    <button>Mount</button>
+    this.setState({m: 2})
+  }
+  render() {
+    console.log('render');
+    return <button onClick={this.update}>
+      {this.state.val * this.state.m}
+    </button>
+  }
+  componentDidMount(){
+    console.log('componentDidMount');
+    // console.log(ReactDOM.findDOMNode(this));
+    this.inc = setInterval(this.update, 500)
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount');
+    clearInterval(this.inc)
+  }
+}
 
-    })
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
   }
   render() {
     return (
       <div>
-        <NumInput 
-          ref="red"
-          min={0}
-          max={255}
-          step={10}
-          val={+this.state.red}
-          type="range"
-          label="Red"
-          update={this.update} />
-
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>Unmount</button>
+        <div id="a"></div>
       </div>
       );
   }
 }
-
-class NumInput extends React.Component {
-  render() {
-    let label = this.props.label !== '' ?
-      <label>{this.props.label} - {this.props.val}</label> : ''
-    return (
-      <div>
-        <input ref="inp" 
-          type={this.props.type}
-          min={this.props.min}
-          max={this.props.max}
-          step={this.props.step}
-          defaultValue={this.props.val}
-          onChange={this.props.update}
-        />
-        {label}
-      </div>
-      );
-  }
-}
-
-NumInput.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  step: React.PropTypes.number,
-  val: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired,
-  type: React.PropTypes.oneOf(['number', 'range'])
-}
-
-NumInput.defaultProps = {
-  min: 0,
-  max: 0,
-  step: 1,
-  val: 0,
-  label: '',
-  type: 'range'
-}
-export default App
+export default Wrapper
